49d574304baec999f140cbda66f00646f0b855b7
svrodela <svrodela@gmail.com>
2025-11-19 12:54:27 -0600

    quire resta

--- practica1.py
+++ practica1.py
@@ -2,9 +2,6 @@ def sumar(a, b):
     """Suma dos números"""
     return a + b
 
-def restar(a, b):
-    """Resta dos números"""
-    return a - b
 
 def multiplicar(a, b):
     """Multiplica dos números"""
